2d34cb2a5b56ac2b555ad92be744c0189e361830
Deepika-J <step.strong.79@gmail.com>
2024-01-27 22:13:28 +0530

    query in graphQL from student data

--- GraphQL/resolver.js
+++ GraphQL/resolver.js
@@ -1,4 +1,8 @@
+const db = require("./db");
+
 const Query = {
-    test: () => 'Test Success, GraphQL server is up & running !!'
- }
- module.exports = {Query}
+  test: () => "Test Success, GraphQL server is up & running !!",
+  students: () => db.students.list(),
+  studentById:(root,args)=> db.students.get(id=args.id)
+};
+module.exports = { Query };
