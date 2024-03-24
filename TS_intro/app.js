64f0ebf7d2f47f28858438486036573e0ce851f1
Deepika-J <step.strong.79@gmail.com>
2024-03-24 20:41:02 +0530

    TS-intro basics

---
+++ TS_intro/app.js
@@ -0,0 +1,6 @@
+var mesage = 'Hello, World!';
+// create a new heading 1 element
+var heading = document.createElement('h1');
+heading.textContent = mesage;
+// add the heading the document
+document.body.appendChild(heading);
